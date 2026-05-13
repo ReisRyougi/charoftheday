d3cd309b9fce1bfb7208825889d81ac1062cd92a
Reis Saigyouji <olegride22@gmail.com>
2026-05-13 02:03:12 +0200

    Added first 4 for testing

---
+++ script.js
@@ -0,0 +1,19 @@
+const characters = [
+    {
+        name: 'Arcueid Brunestud',
+        image: './img/tm/arc.jpg'
+    },
+    {
+        name: 'Artoria Pendragon',
+        image: './img/tm/saber.jpg'
+    },
+    {
+        name: 'Ryougi Shiki',
+        image: './img/tm/ryougi.jpg'
+    },
+    {
+        name: 'Aozaki Aoko',
+        image: './img/tm/aoko.jpg'
+    }
+]
+
